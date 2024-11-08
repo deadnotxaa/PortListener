470a6578608c653368b22aa685fafce57b47a552
bialger <bigulov.sasha@gmail.com>
2024-11-08 16:57:57 +0300

    feat: add commands from config sending

--- bin/main.cpp
+++ bin/main.cpp
@@ -11,12 +11,25 @@ int main(int argc, char** argv) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10'000));
     }
 
-    std::string_view filename = (argc > 2) ? argv[2] : "test.txt";
-                                                          
-	auto listener = port_listener::COMListener(1024, filename, "COM3");
-    listener.sendCommand("LOG TIMEA ONTIME 0.2\r\n");
+    std::string_view input_filename = (argc > 2) ? argv[2] : "test.txt";
+	auto listener = port_listener::COMListener(1024, input_filename, "COM3");
+
+    if (argc > 4) {
+        std::ifstream command_file(argv[4]);
+        std::string command;
+
+        while (std::getline(command_file, command)) {
+            if (command.empty()) {
+                continue;
+            }
+
+            listener.sendCommand(command + "\r\n");
+        }
+    } else {
+        listener.sendCommand("LOG TIMEA ONTIME 0.2\r\n");
+    }
+
     listener.startListening();
-    std::cout << "Enter 'q' to stop listening" << std::endl;
     std::string input;
 
     while (input != "q") {
